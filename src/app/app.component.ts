e1cb74454d2fed4cd4e1427d598f703122b94c47
carlevison <caroline.levison@cloudinary.com>
2025-01-22 14:44:25 +0000

    Add code from video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CloudinaryModule } from '@cloudinary/ng';
+import { Cloudinary, CloudinaryImage } from '@cloudinary/url-gen';
+import { fill } from "@cloudinary/url-gen/actions/resize";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CloudinaryModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Image Gallery';
+  img!: CloudinaryImage;
+
+  ngOnInit(){
+
+    const cld = new Cloudinary({
+      cloud: {
+        cloudName: 'demo'
+      }
+    });
+
+    this.img = cld.image('cld-sample');
+    this.img.resize(fill().width(250).height(250));
+  }
 }
